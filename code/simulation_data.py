7aff0388fcd4e967ff573148c058881e6e3a8d18
kparaschakis <kparaschakis@gnosisda.gr>
2024-01-08 21:57:16 +0200

    minor

--- code/simulation_data.py
+++ code/simulation_data.py
@@ -7,8 +7,8 @@ import os
 alpha_beta_list = [[9, 6], [24, 6]]
 samples_list = [50, 500]
 config_list = [100, 500]
-balance_list = ['equal', 'imbalanced']
-classes_list = [2, 10]
+balance_list = ['equal', 0.1]
+classes_list = [2, 5]
 CI_iter = 200
 # Set seed
 np.random.seed(4321)
@@ -20,11 +20,11 @@ for alpha_beta in alpha_beta_list:
                 for classes in classes_list:
                     alpha, beta = alpha_beta
                     folder_name = 'alpha_' + str(alpha) + '_beta_' + str(beta) + '_samples_' + str(samples) +\
-                                  '_config_' + str(config) + '_balance_' + balance_ + '_classes_' + str(classes)
+                                  '_config_' + str(config) + '_balance_' + str(balance_) + '_classes_' + str(classes)
                     os.makedirs('../simulated_data_for_comparison/' + folder_name)
                     for i in range(CI_iter):
                         balance = np.repeat(1/classes, classes) if balance_ == 'equal' else\
-                            [0.05] + list(np.repeat((1 - 0.05)/(classes - 1), classes - 1))
+                            [balance_] + list(np.repeat((1 - balance_)/(classes - 1), classes - 1))
                         n_folds = min(10, int(samples * min(balance)))
                         predictions_table, outcome, folds, performances =\
                             get_data(alpha, beta, balance, config, samples, folds_=n_folds, type_='multiclass')
