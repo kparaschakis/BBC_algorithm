65f2211a07fc3dd718adfb28a60721b4d08ee0b1
Andrea Castellani <andrea.castellani@honda-ri.de>
2024-03-13 10:40:39 +0100

    remove old files, updated

--- examples.py
+++
@@ -1,8 +0,0 @@
-# Imports
-from functions import *
-
-# Create some example arrays
-distributions = np.random.normal(0, 1, 100000).reshape(100, 1000)
-theoretical = np.random.normal(0, 0.5, 100)
-# Apply percentile uniformity checking function
-percentile_uniformity(distributions, theoretical)
