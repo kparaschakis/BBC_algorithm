6337a29447f9c694b16dc7835c466c5245b43b4f
kparaschakis <kparaschakis@gnosisda.gr>
2024-03-20 13:20:22 +0200

    minor

--- code/BBC_parallel.py
+++ code/BBC_parallel.py
@@ -1,6 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
-#from fastauc.fast_auc import *
+from fastauc.fast_auc import *
 from sklearn.metrics import roc_auc_score
 
 
@@ -8,20 +8,20 @@ def corrcoef2(V1, V2):
     return np.corrcoef(V1, V2)[0, 1] ** 2
 
 
-# def auc_multiclass(outcome, predictions, averaged=True):
-#     auc = CppAuc()
-#     outcome_unique = np.unique(outcome)
-#     performance_vector = 0.5 * np.ones((len(outcome_unique), len(outcome_unique)))
-#     for out_1 in range(len(outcome_unique)):
-#         for out_2 in range(len(outcome_unique)):
-#             if out_1 != out_2:
-#                 performance_vector[out_1, out_2] =\
-#                     auc.roc_auc_score(outcome[np.in1d(outcome, [out_1, out_2])] == out_1,
-#                                       predictions[np.in1d(outcome, [out_1, out_2]), out_1].astype(np.float32))
-#     if averaged:
-#         performance_vector = (np.sum(performance_vector) - np.sum(np.diagonal(performance_vector))) /\
-#                              len(outcome_unique) / (len(outcome_unique)-1)
-#     return performance_vector
+def auc_multiclass(outcome, predictions, averaged=True):
+    auc = CppAuc()
+    outcome_unique = np.unique(outcome)
+    performance_vector = 0.5 * np.ones((len(outcome_unique), len(outcome_unique)))
+    for out_1 in range(len(outcome_unique)):
+        for out_2 in range(len(outcome_unique)):
+            if out_1 != out_2:
+                performance_vector[out_1, out_2] =\
+                    auc.roc_auc_score(outcome[np.in1d(outcome, [out_1, out_2])] == out_1,
+                                      predictions[np.in1d(outcome, [out_1, out_2]), out_1].astype(np.float32))
+    if averaged:
+        performance_vector = (np.sum(performance_vector) - np.sum(np.diagonal(performance_vector))) /\
+                             len(outcome_unique) / (len(outcome_unique)-1)
+    return performance_vector
 
 
 def bbc_pooled(args):
@@ -81,7 +81,7 @@ def bbc_fold(args):
 
 
 def bbc(oos_matrix, labels, analysis_type, folds, bbc_type='pooled', iterations=1000, n_jobs=-1):
-    #auc = CppAuc()
+    auc = CppAuc()
     assert bbc_type in ('pooled', 'averaged', 'fold')
     metric_func = roc_auc_score if analysis_type == 'classification'\
         else corrcoef2 if analysis_type == 'regression' else auc_multiclass
@@ -119,19 +119,5 @@ def bbc(oos_matrix, labels, analysis_type, folds, bbc_type='pooled', iterations=
                 (performance_matrix, F)
             ) for _ in range(iterations)
         )
-        # out_of_bag_performances = []
-        # F = len(np.unique(folds))
-        # performance_matrix = np.zeros((F, C))
-        # for f in range(F):
-        #     for c in range(C):
-        #         performance_matrix[f, c] = metric_func(labels[folds == f], oos_matrix[folds == f, c])
-        # for i in range(iterations):
-        #     in_bag_indices = sorted(np.random.choice(F, F, replace=True))
-        #     while len(in_bag_indices) == len(set(in_bag_indices)):
-        #         in_bag_indices = sorted(np.random.choice(F, F, replace=True))
-        #     out_of_bag_indices = list(set(list(range(F))) - set(in_bag_indices))
-        #     winner_configuration = np.argmax(np.mean(performance_matrix[in_bag_indices, :], axis=0))
-        #     out_of_bag_performances.append(np.mean(performance_matrix[out_of_bag_indices, winner_configuration]))
-        # bbc_distribution = out_of_bag_performances
 
     return np.array(bbc_distribution), winner_configuration
