3c0cfaf9e244e4fda6c581d056506b509868e4ee
kparaschakis <kparaschakis@gnosisda.gr>
2024-04-10 13:45:02 +0300

    minor

--- code/simulation_4_combine_with_R_results.py
+++ code/simulation_4_combine_with_R_results.py
@@ -10,4 +10,4 @@ r_results.index = r_results['configuration']
 r_results.drop('configuration', axis=1, inplace=True)
 results = pd.concat([bbc_results, r_results], axis=1)
 results = results.loc[results.isna().sum(axis=1) == 0]
-results.to_csv('../summaries_all.csv')
+results.to_csv('../simulation_summaries_all.csv')
